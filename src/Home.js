21b3c1f2e274bea64a335d567106a35b92292f43
Joey Bodoia <jbodoia21@cmc.edu>
2020-12-11 14:43:35 -0800

    home page styling

--- src/Home.js
+++ src/Home.js
@@ -12,16 +12,16 @@ const Home = (props) => {
     const {getSongs} = props
 
     const loadedSongs = () => (
-        <div className="songsContainer" style={{"border":"2px solid green", "margin-top": "4vw"}}>
+        <div className="songsContainer" style={{ "margin-top": "4vw"}}>
         {songs.map((song)=> {
           return(
            
-                <div className = "song" style={{"border":"2px solid red"}}>
-                    <h1>{song.title}</h1>
-                    <h2>{song.artist}</h2>
+                <div className = "song" style={{"background-image":`url(${song.coverArt})`, "background-size":"cover"}}>
+                    <h1 style={{"color":"lightgray", "text-shadow":"2px 2px 0 red"}}>{song.title}</h1>
+                    <h2 style={{"color":"white", "text-shadow":"2px 2px 0 red"}}>{song.artist}</h2>
                     <div className="container">
                         <Link to={`/${song.id}`}>
-                            <a>View Song</a>
+                            <button><a>View Song</a></button>
                         </Link>
                         <button onClick={async () => {
                         // make delete request
@@ -41,7 +41,7 @@ const Home = (props) => {
         <div>
             <h1 style={{"margin-top":"2vw", "margin-bottom": "2vw"}}>All Songs</h1>
             <Link to="/songs/new">
-                <a>New Song</a>
+                <button><a>New Song</a></button>
             </Link>
             {songs.length > 0 ? loadedSongs() : <h2>There are no songs!</h2>} 
         </div>
