5630765450755e397c0661e1aa83bb0bfab71311
Joey Bodoia <jbodoia21@cmc.edu>
2020-12-12 11:54:26 -0800

    styling changes on showplaylist page

--- src/Playlist.js
+++ src/Playlist.js
@@ -7,24 +7,24 @@ const Playlist = (props) => {
     const {playlists} = props
 
     const loadedPlaylists = () => (
-        <>
+        <div className="playlistsContainer">
         {playlists.map((playlist)=> {
           return(
-            <div style={{"border":"2px solid black"}}>
-              <h1 style={{"color":"white"}}>{playlist.name}</h1>
-              <Link to={`/playlists/${playlist.id}`}>
-                <a>View Playlist</a>
+            <div style={{"border":"3px solid white", "border-radius":"5px", "width":"50%", "margin-right":"auto", "margin-left":"auto", "margin-bottom":"4vw", "padding-bottom":"2vw", "background-color":"gray"}}>
+              <h1 style={{"color":"white", "font-family":"'Rock Salt', cursive", "font-size":"2vw"}}>{playlist.name}</h1>
+              <Link style={{"text-decoration":"none", "font-family":"'Rock Salt', cursive"}} to={`/playlists/${playlist.id}`}>
+                <a className="viewPlaylistLink">View Playlist</a>
               </Link>
             </div>
           )
         })}
-        </>
+        </div>
       )
 
 
     return(
         <div>
-            <h1>Playlists</h1>
+            <h1 style={{"color":"lightpink", "font-family":"'Rock Salt', cursive", "font-size":"2vw", "margin-bottom":"4vw"}}>Playlists</h1>
             <h2>{playlists.name}</h2>
             {playlists.length > 0 ? loadedPlaylists() : <h2>No Playlists!</h2> }
         </div>
--- src/ShowPlaylist.js
+++ src/ShowPlaylist.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { Route, Link, Switch } from "react-router-dom";
 
 
 const ShowPlaylist = (props) => {
@@ -8,14 +9,16 @@ const ShowPlaylist = (props) => {
     const {songs} = props
     // const {songs} = props.songs
 
+    const {getSongs} = props
+
     const loadPlaylist = () => (
         <>
         {/* {console.log(props.match.params.id)} */}
         {playlists.map((playlist)=> {
             if (playlist.id == props.match.params.id) {
                 return(
-                  <div style={{"border":"2px solid black"}}>
-                    <h1 style={{"color":"white"}}>{playlist.name}</h1>
+                  <div style={{"margin-top":"-3vw"}}>
+                    <h1 style={{"color":"lightpink", "font-family":"'Rock Salt', cursive", "font-size":"3vw"}}>{playlist.name}</h1>
                   </div>
                 )
 
@@ -26,21 +29,39 @@ const ShowPlaylist = (props) => {
     )
 
     const loadPlaylistSongs = () => (
-        <>
+        <div className="songsContainer" style={{ "margin-top": "4vw"}}>
         {songs.map((song)=> {
             if (song.playlists.length>0){
                 console.log(song.playlists[0].id)
                 if (song.playlists[0].id == props.match.params.id) {
                     return(
-                      <div style={{"border":"2px solid black"}}>
-                        <h1 style={{"color":"white"}}>{song.artist}</h1>
-                      </div>
+                        <div className = "song" style={{"background-image":`url(${song.coverArt})`, "background-size":"cover"}}>
+                        <div style={{"border":"2px solid black", "background-color":"gray", "border-radius": "4px"}}>
+                            <h1 style={{"color":"white", "font-family":"'Rock Salt', cursive", "font-size": "1.4vw","border":"2px red", "text-shadow":"1px 1px 0 black"}}>{song.title}</h1>
+                        </div>
+                        <div style={{"border":"2px solid black", "background-color":"lightpink","border-radius": "4px"}}>
+                            <h2 style={{"color":"black", "text-shadow":"1px 1px 0 white","font-family":"'Rock Salt', cursive", "font-size": "1vw"}}>{song.artist}</h2>
+                        </div>
+                        <div className="container">
+                            <Link to={`/${song.id}`}>
+                                <button style={{"font-family":"'Rock Salt', cursive", "font-size":"0.8vw", "border":"2px solid gray", "border-radius":"4px", "background-color": "black", "color": "white"}}><a>View Song</a></button>
+                            </Link>
+                            <button  style={{"font-family":"'Rock Salt', cursive", "font-size":"0.8vw", "background-color":"red", "border":"2px solid black", "border-radius":"4px", "color":"white"}} onClick={async () => {
+                            // make delete request
+                            await fetch("https://rails-songs-playlists-api.herokuapp.com/songs/" + song.id, {
+                            method: "delete"
+                            })
+                            // get updated list of notices
+                            getSongs()
+                            }}>Delete</button>
+                        </div>
+                    </div>
                     )
 
                 }
             }
         })}
-        </>
+        </div>
 
     )
     return(
